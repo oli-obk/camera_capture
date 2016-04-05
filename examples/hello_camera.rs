24c7d7baf30bfecb64a86f785a83459ba8bf8aa1
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-04-05 17:33:49 +0200

    test new builder

--- examples/hello_camera.rs
+++ examples/hello_camera.rs
@@ -2,7 +2,8 @@ extern crate video_input;
 
 fn main() {
     let cam = video_input::create("/dev/video0").unwrap();
-    for _image in cam.start().unwrap() {
+    let cam = cam.fps(5.0).unwrap().start().unwrap();
+    for _image in cam {
         println!("frame");
     }
     println!("done");
