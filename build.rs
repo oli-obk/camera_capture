902ee30a1766e549f98a1c67677b15b447f1ef4c
Oliver Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-04-07 14:09:49 +0200

    unused variable

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ extern crate pkg_config;
 
 fn main() {
     println!("cargo:rerun-if-changed=Cargo.toml");
-    let v4l2 = pkg_config::Config::new().statik(true).probe("libv4l2").expect("library `v4l2` not found");
+    pkg_config::Config::new().statik(true).probe("libv4l2").expect("library `v4l2` not found");
 }
