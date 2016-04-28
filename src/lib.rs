0ff032b0cf46969a460f2610d570efaad5a17438
Oliver 'ker' Schneider <git-spam-no-reply9815368754983@oli-obk.de>
2016-04-28 19:36:34 +0200

    whoops

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,41 @@
 #[cfg(windows)]
 extern crate escapi;
-#[cfg(linux)]
+#[cfg(unix)]
 extern crate rscam;
 #[cfg(windows)]
 #[macro_use]
 extern crate lazy_static;
 extern crate image;
 
-#[cfg(linux)]
-use std::path::Path;
-#[cfg(linux)]
+#[cfg(unix)]
 use std::default::Default;
 
-#[cfg(linux)]
+#[cfg(unix)]
 pub use rscam::Frame;
 #[cfg(windows)]
 pub type Frame = Vec<u8>;
 
 pub struct ImageIterator {
-    #[cfg(linux)]
+    #[cfg(unix)]
     camera: rscam::Camera,
     #[cfg(windows)]
     camera: escapi::Device<'static>,
 }
 
 pub struct Builder {
-    #[cfg(linux)]
+    #[cfg(unix)]
     fps: (u32, u32),
     resolution: (u32, u32),
-    #[cfg(linux)]
+    #[cfg(unix)]
     camera: rscam::Camera,
     #[cfg(windows)]
     camera_id: u32,
 }
 
-#[cfg(linux)]
+#[cfg(unix)]
 pub fn create(i: u32) -> std::io::Result<Builder> {
     Ok(Builder {
-        camera: try!(rscam::Camera::new(format!("/dev/video{}", i))),
+        camera: try!(rscam::Camera::new(&format!("/dev/video{}", i))),
         resolution: (640, 480),
         fps: (1, 10),
     })
@@ -65,7 +63,7 @@ impl From<std::io::Error> for Error {
 }
 
 impl Builder {
-    #[cfg(linux)]
+    #[cfg(unix)]
     pub fn fps(mut self, fps: f64) -> Result<Self, Error> {
         if fps < 5.0 {
             self.fps = (1000, (fps * 1000.0) as u32);
@@ -78,27 +76,31 @@ impl Builder {
             _ => unreachable!(),
         };
         match intervals {
-            rscam::IntervalInfo::Discretes(ref v) => for &(a, b) in v {
-                if a == self.fps.0 && b == self.fps.1 {
-                    return Ok(self);
+            rscam::IntervalInfo::Discretes(ref v) => {
+                for &(a, b) in v {
+                    if a == self.fps.0 && b == self.fps.1 {
+                        return Ok(self);
+                    }
                 }
+                Err(Error::InvalidFps(v.iter().map(|&(a, b)| a as f64 / b as f64).collect()))
             },
             rscam::IntervalInfo::Stepwise { min, max, step } => {
                 if ((self.fps.0 - min.0) / step.0) * step.0 + min.0 == self.fps.0
                 && ((self.fps.1 - min.1) / step.1) * step.1 + min.1 == self.fps.1
                 && max.0 >= self.fps.0
                 && max.1 >= self.fps.1 {
-                    return Ok(self);
+                    Ok(self)
+                } else {
+                    Err(Error::InvalidFps([min, max].iter().map(|&(a, b)| a as f64 / b as f64).collect()))
                 }
             }
         }
-        Err(Error::InvalidFps(intervals))
     }
     #[cfg(windows)]
     pub fn fps(self, _fps: f64) -> Result<Self, Error> {
         Ok(self)
     }
-    #[cfg(linux)]
+    #[cfg(unix)]
     pub fn resolution(mut self, wdt: u32, hgt: u32) -> Result<Self, Error> {
         self.resolution = (wdt, hgt);
         let res = match self.camera.resolutions(b"RGB3") {
@@ -107,28 +109,32 @@ impl Builder {
             _ => unreachable!(),
         };
         match res {
-            rscam::ResolutionInfo::Discretes(ref v) => for &(w, h) in v {
-                if w == wdt && h == hgt {
-                    return Ok(self);
+            rscam::ResolutionInfo::Discretes(ref v) => {
+                for &(w, h) in v {
+                    if w == wdt && h == hgt {
+                        return Ok(self);
+                    }
                 }
+                Err(Error::InvalidResolution(v.clone()))
             },
             rscam::ResolutionInfo::Stepwise { min, max, step } => {
                 if ((wdt - min.0) / step.0) * step.0 + min.0 == wdt
                 && ((hgt - min.1) / step.1) * step.1 + min.1 == hgt
                 && max.0 >= wdt
                 && max.1 >= hgt {
-                    return Ok(self);
+                    Ok(self)
+                } else {
+                    Err(Error::InvalidResolution(vec![min, max]))
                 }
             }
         }
-        Err(Error::InvalidResolution(res))
     }
     #[cfg(windows)]
     pub fn resolution(mut self, wdt: u32, hgt: u32) -> Result<Self, Error> {
         self.resolution = (wdt, hgt);
         Ok(self)
     }
-    #[cfg(linux)]
+    #[cfg(unix)]
     pub fn start(mut self) -> std::io::Result<ImageIterator> {
         match self.camera.start(&rscam::Config {
             interval: self.fps,      // 30 fps.
@@ -155,7 +161,7 @@ impl Builder {
     }
 }
 
-#[cfg(linux)]
+#[cfg(unix)]
 impl Iterator for ImageIterator {
     type Item = image::ImageBuffer<image::Rgb<u8>, Frame>;
     fn next(&mut self) -> Option<Self::Item> {
